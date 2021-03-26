74229d9b1557ae9741b78dc1ef43a4183af55b1c
vasisht54 <vasisht.theviper@gmail.com>
2021-03-26 13:54:27 -0700

    Added links to pet-search and create-listing

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <div className="Home">
       <h4>Home</h4>
+      <div>
+        <Link to="/search">Meet Pets</Link>
+      </div>
+      <div>
+        <Link to="/create">Create a Pet Listing</Link>
+      </div>
     </div>
   );
 };
