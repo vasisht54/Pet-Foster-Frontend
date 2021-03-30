1edb9c3dc558ddb0055dcce14474a0cbdcdde2ff
nesaramadhav <madhav.n@husky.neu.edu>
2021-03-30 19:09:53 -0400

    send foster request

--- src/PetSearch/PetSearch.js
+++ src/PetSearch/PetSearch.js
@@ -10,7 +10,7 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
-
+import SendFosterRequest from "./SendFosterRequest";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 
 import { Filters } from "./PetSearchFilters";
@@ -124,8 +124,8 @@ function PetSearch() {
                 </CardContent>
               </CardActionArea>
               <CardActions>
-                <Button size="small" color="primary">
-                  Send foster request
+                <Button size="small" color="primary" href="/petSearchDetails" >
+                  View Pet Details
                 </Button>
               </CardActions>
             </Card>
