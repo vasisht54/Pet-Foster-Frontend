ce3d6e2f0a84f11be4cbec01397dfdf892d0a80e
nesaramadhav <madhav.n@husky.neu.edu>
2021-03-31 22:53:55 -0400

    removed unused libraries

--- src/PetListing/ViewFosterRequestForMyPet.js
+++ src/PetListing/ViewFosterRequestForMyPet.js
@@ -4,19 +4,9 @@ import Button from "@material-ui/core/Button";
 import GridList from "@material-ui/core/GridList";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
-import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
-import Dialog from "@material-ui/core/Dialog";
 import MuiDialogContent from "@material-ui/core/DialogContent";
 import MuiDialogActions from "@material-ui/core/DialogActions";
-import Paper from '@material-ui/core/Paper';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import FosterRequestTable from './FosterRequestTable';
+
 
 const drawerWidth = 300;
 
