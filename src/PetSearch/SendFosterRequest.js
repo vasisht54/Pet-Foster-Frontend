459b20eee72e568bc0039fe572fbd4ac3794b889
nesaramadhav <madhav.n@husky.neu.edu>
2021-04-02 13:52:03 -0400

    formated foster request form

--- src/PetSearch/SendFosterRequest.js
+++ src/PetSearch/SendFosterRequest.js
@@ -12,6 +12,7 @@ import Button from "@material-ui/core/Button";
 import Dialog from '@material-ui/core/Dialog';
 import MuiDialogContent from '@material-ui/core/DialogContent';
 import MuiDialogActions from '@material-ui/core/DialogActions';
+import { Grid } from "@material-ui/core";
 
 const drawerWidth = 300;
 const useStyles = makeStyles((theme) => ({
@@ -79,85 +80,184 @@ function SendFosterRequest() {
   };
 
   return (
-    <div className={classes.root}>
-      <Container component="main" maxWidth="xs">
-        <Paper className={classes.paper} elevation={3}>
-          <div className={classes.Card}>
-            <Typography gutterBottom variant="h4">
-              Foster Request
-            </Typography>
-          </div>
-          <div className={classes.Card}>
-            <TextField
-              id="outlined-multiline-static"
-              label="Message For the Owner (Optional)"
-              multiline
-              rows={4}
-              variant="outlined"
-            />
-          </div>
-          <FormGroup aria-label="position" row>
-            <FormControlLabel
-              value="end"
-              control={<Checkbox color="primary" />}
-              label="I acknowledge that this is a temporary foster period and that I will have to return the fostered pet to it's owner."
-              labelPlacement="end"
-            />
-          </FormGroup>
-          <FormGroup aria-label="position" row>
-            <FormControlLabel
-              value="end"
-              control={<Checkbox color="primary" />}
-              label="I acknowledge that this is a volunteer work and I will not ask for any monetory reward from the owner for taking care of their pet."
-              labelPlacement="end"
-            />
-          </FormGroup>
-          <div>
-            <label htmlFor="contained-button-file">
-              <Button
-                variant="contained"
-                color="primary"
-                component="span"
-                onClick={handleOpen}
-              >
-                Send Request
-              </Button>
-            </label>
-            <Dialog
-              onClose={handleClose}
-              aria-labelledby="customized-dialog-title"
-              open={open}
-            >
-              <DialogContent dividers>
-                <Typography gutterBottom>
-                  Kudos! Foster Request sent to the owner successfully.
-                </Typography>
-                <Typography gutterBottom>
-                    Now wait for them to accept it!
-                </Typography>
-              </DialogContent>
-              <DialogActions>
-                <Button autoFocus onClick={()=> {handleClose(); history.push("/search")}} color="primary">
-                  Okay
+<>
+      <div className={classes.Card} style = {{minHeight: 50}} >
+      </div>
+    <Container>
+      <Grid container >
+        <Grid item xs={1} />
+        <Grid item xs={10} container direction="column">
+        <Grid container item >
+          <Paper className={classes.paper} elevation={3}>
+            <div className={classes.Card}>
+              <Typography gutterBottom variant="h4">
+                Foster Request
+              </Typography>
+            </div>
+            <div className={classes.Card}>
+              <TextField
+                id="outlined-multiline-static"
+                label="Message For the Owner (Optional)"
+                multiline
+                rows={4}
+                style = {{width: 800}}
+                variant="outlined"
+              />
+            </div>
+            <FormGroup aria-label="position" row>
+              <FormControlLabel
+                value="end"
+                control={<Checkbox color="primary" />}
+                label="I acknowledge that this is a temporary foster period and that I will have to return the fostered pet to it's owner."
+                labelPlacement="end"
+              />
+            </FormGroup>
+            <FormGroup aria-label="position" row>
+              <FormControlLabel
+                value="end"
+                control={<Checkbox color="primary" />}
+                label="I acknowledge that this is a volunteer work and I will not ask for any monetory reward from the owner for taking care of their pet."
+                labelPlacement="end"
+              />
+            </FormGroup>
+            
+            <Grid item className={classes.actionButtons} container xs={6}>
+              <Grid item xs={6}>
+              
+                <Button
+                  variant="contained"
+                  color="primary"
+                  component="span"
+                  onClick={handleOpen}
+                  style={{justifyContent: 'center', left: 250}}
+                >
+                  Send Request
                 </Button>
-              </DialogActions>
-            </Dialog>
-            {/* ************************************************************* */}
+              
+              <Dialog
+                onClose={handleClose}
+                aria-labelledby="customized-dialog-title"
+                open={open}
+              >
+                <DialogContent dividers>
+                  <Typography gutterBottom>
+                    Kudos! Foster Request sent to the owner successfully.
+                  </Typography>
+                  <Typography gutterBottom>
+                      Now wait for them to accept it!
+                  </Typography>
+                </DialogContent>
+                <DialogActions>
+                  <Button autoFocus onClick={()=> {handleClose(); history.push("/search")}} color="primary">
+                    Okay
+                  </Button>
+                </DialogActions>
+              </Dialog>
+              </Grid>
+              {/* ************************************************************* */}
+              <Grid item xs={6} style={{justifyContent: 'center'}}>
+              <Button
+                onClick={()=> {
+                  history.push("/PetSearchDetails")
+                }}
+                variant="contained"
+                color="secondary"
+                className={classes.button}
+                style={{justifyContent: 'center', left: 250}}
+              >
+                Cancel
+              </Button>
+              </Grid>
+              </Grid>
+            
+          </Paper>
+          </Grid>
+        </Grid>
+        </Grid>
 
-            <Button
-              onClick={()=> {
-                history.push("/PetSearchDetails")
-              }}
-              variant="contained"
-              color="secondary"
-              className={classes.button}
-            >
-              Cancel
-            </Button>
-          </div>
-        </Paper>
-      </Container>
-    </div>
+</Container>
+</>
+
+
+    // <div className={classes.root}>
+    //   <Container component="main" maxWidth="xs">
+    //     <Paper className={classes.paper} elevation={3}>
+    //       <div className={classes.Card}>
+    //         <Typography gutterBottom variant="h4">
+    //           Foster Request
+    //         </Typography>
+    //       </div>
+    //       <div className={classes.Card}>
+    //         <TextField
+    //           id="outlined-multiline-static"
+    //           label="Message For the Owner (Optional)"
+    //           multiline
+    //           rows={4}
+    //           variant="outlined"
+    //         />
+    //       </div>
+    //       <FormGroup aria-label="position" row>
+    //         <FormControlLabel
+    //           value="end"
+    //           control={<Checkbox color="primary" />}
+    //           label="I acknowledge that this is a temporary foster period and that I will have to return the fostered pet to it's owner."
+    //           labelPlacement="end"
+    //         />
+    //       </FormGroup>
+    //       <FormGroup aria-label="position" row>
+    //         <FormControlLabel
+    //           value="end"
+    //           control={<Checkbox color="primary" />}
+    //           label="I acknowledge that this is a volunteer work and I will not ask for any monetory reward from the owner for taking care of their pet."
+    //           labelPlacement="end"
+    //         />
+    //       </FormGroup>
+    //       <div>
+    //         <label htmlFor="contained-button-file">
+    //           <Button
+    //             variant="contained"
+    //             color="primary"
+    //             component="span"
+    //             onClick={handleOpen}
+    //           >
+    //             Send Request
+    //           </Button>
+    //         </label>
+    //         <Dialog
+    //           onClose={handleClose}
+    //           aria-labelledby="customized-dialog-title"
+    //           open={open}
+    //         >
+    //           <DialogContent dividers>
+    //             <Typography gutterBottom>
+    //               Kudos! Foster Request sent to the owner successfully.
+    //             </Typography>
+    //             <Typography gutterBottom>
+    //                 Now wait for them to accept it!
+    //             </Typography>
+    //           </DialogContent>
+    //           <DialogActions>
+    //             <Button autoFocus onClick={()=> {handleClose(); history.push("/search")}} color="primary">
+    //               Okay
+    //             </Button>
+    //           </DialogActions>
+    //         </Dialog>
+    //         {/* ************************************************************* */}
+
+    //         <Button
+    //           onClick={()=> {
+    //             history.push("/PetSearchDetails")
+    //           }}
+    //           variant="contained"
+    //           color="secondary"
+    //           className={classes.button}
+    //         >
+    //           Cancel
+    //         </Button>
+    //       </div>
+    //     </Paper>
+    //   </Container>
+    // </div>
   );
 }
 export default SendFosterRequest;
