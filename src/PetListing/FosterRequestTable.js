216fb89a9e26165ce5fe7bdb8cde3ce7203ec912
vasisht54 <vasisht.theviper@gmail.com>
2021-04-20 22:49:01 -0700

    Removed unused code

--- src/PetListing/FosterRequestTable.js
+++ src/PetListing/FosterRequestTable.js
@@ -72,42 +72,6 @@ const useStyles = makeStyles(theme => ({
   item: { margin: "20px" },
 }));
 
-// const fosterers = [
-//   {
-//     id: 1,
-//     name: "Jackson O'Doherty",
-//     image: "/static/images/avatar.png",
-//     rating: "4.9/5.0",
-//     phoneNum: "123-456-7890",
-//     email: "jackson@odoherty.com",
-//     bio: "I live for pet animals!",
-//     message:
-//       "I love dogs and I have one myself - a Retriever. He's good with other dogs, hence I'm willing to foster your pet as well.",
-//   },
-//   {
-//     id: 2,
-//     name: "John Krasinski",
-//     image: "/static/images/avatar2.jpg",
-//     rating: "4.2/5.0",
-//     phoneNum: "123-456-7890",
-//     email: "John@krasinski.com",
-//     bio: "Grew up with 3 dogs and a cat. Love pets!",
-//     message:
-//       "I grew up with 3 dogs, 2 cats and a horse. I'm very good with pets.",
-//   },
-//   {
-//     id: 3,
-//     name: "Matthew Perry",
-//     image: "/static/images/avatar3.jpg",
-//     rating: "4.7/5.0",
-//     phoneNum: "123-456-7890",
-//     email: "chandler@bing.com",
-//     bio: "Great with pets!",
-//     message:
-//       "I love dogs and I have one myself. Willing to take care of yours - please reach out.",
-//   },
-// ];
-
 const FormRow = props => {
   return (
     <Grid container item xs={12}>
